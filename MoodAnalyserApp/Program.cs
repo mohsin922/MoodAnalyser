e58afe890eb84f42dba5710eaed8ed206592e051
peermohsin22 <peermohsin22@gmail.com>
2022-01-09 02:34:00 +0530

    Welcome To MoodAnalyser App

---
+++ MoodAnalyserApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser App!");
+        }
+    }
+}
