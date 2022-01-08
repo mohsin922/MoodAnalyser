3b3209d50577ea7b40172aa12573c73ca2e89578
peermohsin22 <peermohsin22@gmail.com>
2022-01-09 03:40:19 +0530

    MoodAnalyser Class Should Return MoodAnalyser Object Using Reflection by Default Constructor

---
+++ MoodAnalyserApp/MoodAnalyserFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserApp
+{
+    public class MoodAnalyserFactory
+    {
+        ///<summary>
+        ///CreatMoodAnalyser method to create object of MoodAnalyser class.
+        /// </summary>
+        public static Object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+    }
+}
